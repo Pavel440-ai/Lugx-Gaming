17c55ac0bb450aac2d1e822b870f8ca09ca957e4
Pavel <potapenkopavel343@gmail.com>
2025-03-25 22:42:26 +0100

    HeaderPHP
    Made a folder with separate header.php and navigation

---
+++ php/header.php
@@ -0,0 +1,11 @@
+<?php ?>
+<header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <?php include 'ComponentsHeader\nav.php'; ?>
+            </div>
+        </div>
+    </div>
+</header>
+
